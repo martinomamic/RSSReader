c3ae798c1ad2a9532d75f947db0c90b2fe4af201
Martino Mamic <martino.mamic@intive.com>
2025-04-12 13:38:27 +0200

    Initial commit

---
+++ RSSReader/RSSReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSReaderApp.swift
+//  RSSReader
+//
+//  Created by Martino Mamić on 12.04.25.
+//
+
+import SwiftUI
+
+@main
+struct RSSReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RSSReaderTests/RSSReaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  RSSReaderTests.swift
+//  RSSReaderTests
+//
+//  Created by Martino Mamić on 12.04.25.
+//
+
+import Testing
+
+struct RSSReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
